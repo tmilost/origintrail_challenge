72099575b6c8620abe89b600000fad0cf05cc25e
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2021-01-07 09:12:03 +0100

    change font

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;

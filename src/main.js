11e7a25273f3cb33cfc22b176fe45ca5b361fce5
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2021-01-07 09:48:18 +0100

    remove axios import

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from './App.vue'
 import router from './router'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-import axios from 'axios'
+
 
 
 Vue.use(Element)
-Vue.use(axios)
+
 
 Vue.config.productionTip = false
 

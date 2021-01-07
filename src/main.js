e50792c9eee39c1e6e1faa95d335174113e6e0db
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2021-01-07 06:09:10 +0100

    import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+
+
+Vue.use(Element)
+Vue.use(axios)
 
 Vue.config.productionTip = false
 

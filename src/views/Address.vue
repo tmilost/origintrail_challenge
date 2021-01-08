fd91e0724c1e1a07e5816c60776e30233c7abaeb
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2021-01-08 19:25:34 +0100

    add internal table

--- src/views/Address.vue
+++ src/views/Address.vue
@@ -26,7 +26,7 @@
        <el-card class="box-card" shadow="hover">
       
       <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="Transactions" name="first">
+    <el-tab-pane label="Transactions" name="first" >
           <el-table
     :data="NormalTransactions"
     height="590"
@@ -75,9 +75,58 @@
   :total="1000">
 </el-pagination>
      </el-tab-pane>
-    <el-tab-pane label="Internal Txns" name="second">Config</el-tab-pane>
-    <el-tab-pane label="Erc20 Token Txns" name="third">Role</el-tab-pane>
-    <el-tab-pane label="ERC721 Token Txns" name="fourth">Task</el-tab-pane>
+    <el-tab-pane label="Internal Txns" name="second">
+      
+             <el-table
+    :data="InternalTransactions"
+    height="590"
+    style="width: 100%">
+     <el-table-column
+      prop="hash"
+      label="Txn Hash"
+      width="280">
+    </el-table-column>
+        <el-table-column
+      prop="blockNumber"
+      label="Block"
+      width="280">
+    </el-table-column>
+     <el-table-column
+      label="Time"
+      width="280">
+       <template slot-scope="scope">
+        {{ timeConverter(scope.row.timeStamp) }}
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="from"
+      label="From"
+      width="280">
+    </el-table-column>
+    <el-table-column
+      prop="to"
+      label="To"
+      width="280">
+    </el-table-column>
+    <el-table-column
+      label="Ether"
+      width="380">
+      <template slot-scope="scope">
+        {{ scope.row.value / 1000000000000000000 }}
+      </template>
+    </el-table-column>
+   
+  </el-table>
+   <el-pagination
+  background
+  layout="prev, pager, next"
+   @current-change="handleCurrentChange"
+   v-on:change="handleCurrentChange(handleCurrentChange)"
+  :total="1000">
+</el-pagination>
+
+    </el-tab-pane>
+   
   </el-tabs>
 
     </el-card>
@@ -87,6 +136,8 @@
 
 <script>
 import axios from "axios";
+import config from "../../public/config.json";
+
 export default {
   props: {
     address: String,
@@ -94,11 +145,12 @@ export default {
     endblock: String,
   },
   data: () => ({
+    config: config,
     balance: 0,
     etherPriceUsd: 0,
      activeName: 'first',
      NormalTransactions:[],
-       etherPaasdsdriceUsd: 0,
+     InternalTransactions:[],
   }),
   methods: {
      timeConverter(UNIX_timestamp){
@@ -129,30 +181,37 @@ export default {
         .get(
           "https://api.etherscan.io/api?action=balance&address=" +
             this.address +
-            "&tag=latest&module=account&apikey=568E6J3J3XHJZASJV21UFWDF8YKJG2G3JM"
+            "&tag=latest&module=account&apikey="+config.apikey
         )
         .then((response) => (this.balance = response.data.result / 1000000000000000000 ));
     },
     getEthPrice() {
       axios
         .get(
-          "https://api.etherscan.io/api?module=stats&action=ethprice&apikey=568E6J3J3XHJZASJV21UFWDF8YKJG2G3JM"
+          "https://api.etherscan.io/api?module=stats&action=ethprice&apikey="+config.apikey
         )
         .then((response) => (this.etherPriceUsd = response.data.result.ethusd));
     },
     getNormalTransactions(page){
          axios
         .get(
-          "https://api.etherscan.io/api?module=account&action=txlist&address=" + this.address + "&startblock="+this.startblock+"&endblock="+this.endblock+"&page="+page+"&offset=10&sort=asc&apikey=568E6J3J3XHJZASJV21UFWDF8YKJG2G3JM"
+          "https://api.etherscan.io/api?module=account&action=txlist&address=" + this.address + "&startblock="+this.startblock+"&endblock="+this.endblock+"&page="+page+"&offset=10&sort=asc&apikey="+config.apikey
         )
         .then((response) => (this.NormalTransactions = response.data.result));
     },
-    
+    getInternalTransactions(page){
+         axios
+        .get(
+          "https://api.etherscan.io/api?module=account&action=txlistinternal&address=" + this.address + "&startblock="+this.startblock+"&endblock="+this.endblock+"&page="+page+"&offset=10&sort=asc&apikey="+config.apikey
+        )
+        .then((response) => (this.InternalTransactions = response.data.result));
+    },
   },
   mounted() {
     this.getBalance();
     this.getEthPrice();
       this.getNormalTransactions(1);
+      this.getInternalTransactions(1);
     
   },
 };

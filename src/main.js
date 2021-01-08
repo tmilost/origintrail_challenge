82ad316689884dccd6a6217feaa40aaa507f1105
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2021-01-08 14:20:02 +0100

    add awsome icon

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
-
+import 'element-ui/lib/theme-chalk/index.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 
 Vue.use(Element)

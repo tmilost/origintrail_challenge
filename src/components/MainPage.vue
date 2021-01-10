af7ea41e345211b7209277359f239290a1518d33
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2021-01-10 17:12:58 +0100

    price currency color change

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -60,7 +60,7 @@
           <div style="float:left; padding-right:15px;font-size:2rem;">₿</div>
           <div class="divTableBody">
             <div class="divTableRow">
-              <div class="divTableCell">Btc</div>
+              <div class="divTableCell"><h7>BTC</h7></div>
             </div>
             <div class="divTableRow">
               <div class="divTableCell">{{ etherPrice.ethbtc }}</div>
@@ -74,7 +74,7 @@
           <div style="float:left; padding-right:15px;font-size:2rem;">🇺🇸</div>
           <div class="divTableBody">
             <div class="divTableRow">
-              <div class="divTableCell">Usd</div>
+              <div class="divTableCell"><h7>USD</h7></div>
             </div>
             <div class="divTableRow">
               <div class="divTableCell">{{ etherPrice.ethusd }}</div>
@@ -88,7 +88,7 @@
           <div style="float:left; padding-right:15px;font-size:2rem;">🇪🇺</div>
           <div class="divTableBody">
             <div class="divTableRow">
-              <div class="divTableCell">Eur</div>
+              <div class="divTableCell"><h7>EUR</h7></div>
             </div>
             <div class="divTableRow">
               <div class="divTableCell">
@@ -104,7 +104,7 @@
           <div style="float:left; padding-right:15px;font-size:2rem;">🇳🇴</div>
           <div class="divTableBody">
             <div class="divTableRow">
-              <div class="divTableCell">Nok</div>
+              <div class="divTableCell"><h7>NOK</h7></div>
             </div>
             <div class="divTableRow">
               <div class="divTableCell">
@@ -120,7 +120,7 @@
           <div style="float:left; padding-right:15px;font-size:2rem;">🇨🇦</div>
           <div class="divTableBody">
             <div class="divTableRow">
-              <div class="divTableCell">CAD</div>
+              <div class="divTableCell"><h7>CAD</h7></div>
             </div>
             <div class="divTableRow">
               <div class="divTableCell">
@@ -137,7 +137,7 @@
 
           <div class="divTableBody">
             <div class="divTableRow">
-              <div class="divTableCell">HKD</div>
+              <div class="divTableCell"><h7>HKD</h7></div>
             </div>
             <div class="divTableRow">
               <div class="divTableCell">
@@ -368,5 +368,8 @@ p {
   color: #77838f;
   margin: 0px;
   font-size: 1.21875rem;
+}
+h7 {
+  color: #77838f;
 }
 </style>
